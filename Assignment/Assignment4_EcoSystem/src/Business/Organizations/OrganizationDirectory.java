07da07d154c7aa79a11e1676b37c795cf1b87c4b
TengfeiWong <wang.ten@northeastern.edu>
2021-03-26 14:03:43 +0800

    assignment4 log in

---
+++ Assignment/Assignment4_EcoSystem/src/Business/Organizations/OrganizationDirectory.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Business.Organizations;
+
+import Business.Organizations.Organization.Type;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author NURES
+ */
+public class OrganizationDirectory {
+        
+    private ArrayList<Organization> organizationList;
+
+    public OrganizationDirectory() {
+        organizationList = new ArrayList<>();
+    }
+
+    public ArrayList<Organization> getOrganizationList() {
+        return organizationList;
+    }
+    
+    public Organization createOrganization(Type type){
+        Organization organization = null;
+        if (type.getValue().equals(Type.SysAdmin.getValue())){
+            organization = new SystemAdminOrganization();
+            organizationList.add(organization);
+        }
+        else if (type.getValue().equals(Type.RestaurantAdmin.getValue())){
+            organization = new RestaurantOrganization();
+            organizationList.add(organization);
+        }
+        return organization;
+    }
+}
